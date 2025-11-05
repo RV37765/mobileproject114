5af26b8e0eaf9265643f189c5348040f84fb830c
Ryan Varghese <rtv37765@uga.edu>
2025-11-04 23:24:17 -0500

    some working features. need to fix things i texted

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/tasks/AsyncTask.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/tasks/AsyncTask.java
@@ -1,80 +1,62 @@
-package edu.uga.cs.jobstrackersqlite;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+package edu.uga.cs.statecapitalsquiz.tasks;
 
 import android.os.Handler;
 import android.os.Looper;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-/** This is a replacement class for the Android's  AsyncTask class.
- * However, handling of the task's progress has not been implemented.
- * It uses Java's standard concurrency framework.
- * @param <Param> type of the input parameter for doInBackground
- * @param <Result> type of the result value returned by doInBackground
+/**
+ * A custom implementation of the deprecated AsyncTask to be used in modern Android versions.
+ * This class allows background operations and publishing results on the UI thread.
+ *
+ * @param <Params>   the type of the parameters sent to the task upon execution.
+ * @param <Progress> the type of the progress units published during the background computation.
+ * @param <Result>   the type of the result of the background computation.
  */
-public abstract class AsyncTask<Param, Result> {
+public abstract class AsyncTask<Params, Progress, Result> {
 
-    // An internal method to execute something in background
-    private void executeInBackground( Param... params ) {
+    private static final String TAG = "AsyncTask";
 
-        // Get en executor service -- it will run the task in the
-        // background, i.e., in this executor.  This executor service
-        // has its own thread, which is different than the main UI thread.
-        // As a result, anything running on this thread will not block the UI.
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+    private final Executor executor = Executors.newSingleThreadExecutor();
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
-        // Execute a Runnable with the task on the executor service.
-        // A Runnable is a Java interface to be implemented by a class to execute in
-        // a thread.  The run() method is called automatically when a Thread is given
-        // a Runnable object.
-        //
-        // The lambda expression below effectively creates an anonymous
-        // Runnable class that will execute the method's body (doInBackground)
-        // in the executor service, which uses a different thread than the main UI thread.
-        // Once the result is obtained, the Runnable class will add another Runnable
-        // with the call to onPostExecute with the Result argument to be added to the
-        // main UI thread.  The main UI thread will update the UI accordingly.
-        // Again, since the Runnable below will execute in a different thread, the main
-        // UI thread will not be blocked.
-        executor.execute( () -> {
+    /**
+     * This method is invoked on the background thread immediately after onPreExecute()
+     * and before onPostExecute().
+     *
+     * @param params The parameters of the task.
+     * @return A result, defined by the subclass of this task.
+     */
+    protected abstract Result doInBackground(Params... params);
 
-            // Run the method body (doInBackground)
-            Result result = doInBackground( params );
+    /**
+     * Runs on the UI thread before doInBackground().
+     */
+    protected void onPreExecute() {
+        // Default is no-op
+    }
 
-            // Now, pass the result to the main UI thread.
-            //
-            // Get the Looper of the UI thread (the main UI event dispatcher's loop).
-            // A Looper is simply a message queue within the Android OS.
-            // If you are curious, you can examine the source code of the Looper class here:
-            // https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/os/Looper.java
-            Looper looper = Looper.getMainLooper();
+    /**
+     * Runs on the UI thread after doInBackground(). The specified result is the value
+     * returned by doInBackground().
+     *
+     * @param result The result of the operation computed by doInBackground().
+     */
+    protected void onPostExecute(Result result) {
+        // Default is no-op
+    }
 
-            // Create a Handler using the main UI's looper.
-            // A Handler is used to interact with a Looper, for example,
-            // for posting messages on the main Looper's queue.
-            // If you are curious, you can examine the source code of the Handler class here:
-            // https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/os/Handler.java
-            Handler handler = new Handler( looper );
-
-            // Post the processing of the result of the doInBackground method
-            // on the main UI thread's looper. Android will process it.
-            handler.post( new Runnable() {
-                @Override
-                public void run() {
-                    // handle the method result in the main UI thread
-                    onPostExecute( result );
-                }
-            });
+    /**
+     * Executes the task with the given parameters. This method must be invoked on the UI thread.
+     *
+     * @param params The parameters of the task.
+     */
+    public final void execute(Params... params) {
+        onPreExecute();
+        executor.execute(() -> {
+            Result result = doInBackground(params);
+            handler.post(() -> onPostExecute(result));
         });
     }
-
-    // This method is just like in the AsyncTask class
-    public void execute( Param... arguments ){
-        executeInBackground( arguments );
-    }
-
-    // These abstract methods are just like in the AsyncTask class
-    protected abstract Result doInBackground( Param... arguments );
-    protected abstract void onPostExecute( Result result );
 }
